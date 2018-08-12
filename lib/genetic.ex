4cca529859f21d62d619ae45cb784b16dd90d26d
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-12 22:26:09 +0200

    Merge private Genetic.Populate/3 into public one

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -1,16 +1,12 @@
 defmodule Genetic do
   def populate(number, allowed_values, length) when number > 0 and length > 0,
-    do: _populate(number, allowed_values, length, %Chromosome{})
+    do: for(_ <- 1..number, do: random_chromosome(allowed_values, length))
 
   def populate(_, _, _), do: []
 
   def crossover(population)
   def mutate(population, chance)
   def calc_fitness(population, fitness_func)
-
-  defp _populate(number, allowed_values, length, population) do
-    for _ <- 1..number, do: random_chromosome(allowed_values, length)
-  end
 
   defp random_chromosome(allowed_values, length) do
     genes = for _ <- 1..length, do: Enum.random(allowed_values)

144451ec76430b79314cffe3d88234cf61f0dcdf
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-12 22:04:24 +0200

    Implement Genetic.populate/3

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -1,6 +1,17 @@
 defmodule Genetic do
-  def populate(number, allowed_values)
+  def populate(number, allowed_values, length) when number >= 0 and length >= 0,
+    do: _populate(number, allowed_values, length, %Chromosome{})
+
   def crossover(population)
   def mutate(population, chance)
   def calc_fitness(population, fitness_func)
+
+  defp _populate(number, allowed_values, length, population) do
+    for _ <- 1..number, do: random_chromosome(allowed_values, length)
+  end
+
+  defp random_chromosome(allowed_values, length) do
+    genes = for _ <- 1..length, do: Enum.random(allowed_values)
+    %Chromosome{genes: genes}
+  end
 end

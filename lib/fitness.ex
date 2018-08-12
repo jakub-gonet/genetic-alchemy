3ed650d66019ca82da9223052c3b15df6e45fddd
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-12 20:32:00 +0200

    Add Fitness module stub

---
+++ lib/fitness.ex
@@ -0,0 +1,8 @@
+defmodule Fitness do
+  @moduledoc """
+  This module is responsible for providing function calculate/1
+  which calculates fittness of given Chromosome
+  """
+
+  def calculate(chromosome = %Chromosome{}), do: 1
+end

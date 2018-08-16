e632ff47a426869646548dea45f32b58047a2879
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-16 22:47:33 +0200

    Rename random_chromosome/2

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -1,6 +1,6 @@
 defmodule Genetic do
   def populate(number, allowed_values, length) when number > 0 and length > 0,
-    do: for(_ <- 1..number, do: random_chromosome(allowed_values, length))
+    do: for(_ <- 1..number, do: generate_random_chromosome(allowed_values, length))
 
   def populate(_, _, _), do: []
 
@@ -15,7 +15,7 @@ defmodule Genetic do
   def mutate(population, chance)
   def calc_fitness(population, fitness_func)
 
-  defp random_chromosome(allowed_values, length) do
+  defp generate_random_chromosome(allowed_values, length) do
     genes = for _ <- 1..length, do: Enum.random(allowed_values)
     %Chromosome{genes: genes}
   end

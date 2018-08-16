fc4b970c0215ebb46b5153289b970046664b1c4a
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-16 22:17:48 +0200

    Add function for crossovering two chromosomes

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -13,5 +13,18 @@ defmodule Genetic do
     %Chromosome{genes: genes}
   end
 
+  def crossover_two([%Chromosome{genes: first}, %Chromosome{genes: second}]) do
+    merge_swapped = fn {a_1, a_2}, {b_1, b_2} -> {a_1 ++ b_2, b_1 ++ a_2} end
+
+    crossover_point = crossover_point_from(length(first))
+    first_split = Enum.split(first, crossover_point)
+    second_split = Enum.split(second, crossover_point)
+
+    {first_m, second_m} = merge_swapped.(first_split, second_split)
+    [%Chromosome{genes: first_m}, %Chromosome{genes: second_m}]
+  end
+
+  def crossover_two(first), do: first
+
   defp crossover_point_from(length), do: Enum.random(1..length)
 end

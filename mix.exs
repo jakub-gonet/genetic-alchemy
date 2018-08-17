feac9315635da0be3d99405346528734f13d0a86
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-17 10:44:20 +0200

    Add excoveralls as coverage reporter

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Genetic.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule Genetic.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:flow, "~> 0.14"}
+      {:flow, "~> 0.14"},
+      {:excoveralls, "~> 0.9", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

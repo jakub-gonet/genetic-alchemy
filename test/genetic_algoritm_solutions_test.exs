5f6d5511c7a3bd9d7cd1d0f15dfcde8f7095b42c
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 18:16:33 +0200

    Move fitness function in test

--- test/genetic_algoritm_solutions_test.exs
+++ test/genetic_algoritm_solutions_test.exs
@@ -2,18 +2,6 @@ defmodule GeneticAlgoritmSolutionTest do
   use ExUnit.Case, async: true
   require Logger
 
-  def pattern_fitness(genes) do
-    desired = [%Gene{v: 1}, %Gene{v: 2}, %Gene{v: 3}, %Gene{v: 4}, %Gene{v: 5}]
-    count = Enum.count(genes)
-
-    fitting =
-      desired
-      |> Enum.zip(genes)
-      |> Enum.reduce(0, fn {x, y}, acc -> if(x == y, do: 1, else: 0) + acc end)
-
-    fitting / count
-  end
-
   describe "GeneticAlgorithm.find_solution/2" do
     test "finding specific pattern" do
       desired = %Chromosome{
@@ -34,5 +22,17 @@ defmodule GeneticAlgoritmSolutionTest do
       Logger.info("Found solution after #{solution.generations} generations")
       assert desired in solution.most_fitting
     end
+  end
+
+  def pattern_fitness(genes) do
+    desired = [%Gene{v: 1}, %Gene{v: 2}, %Gene{v: 3}, %Gene{v: 4}, %Gene{v: 5}]
+    count = Enum.count(genes)
+
+    fitting =
+      desired
+      |> Enum.zip(genes)
+      |> Enum.reduce(0, fn {x, y}, acc -> if(x == y, do: 1, else: 0) + acc end)
+
+    fitting / count
   end
 end

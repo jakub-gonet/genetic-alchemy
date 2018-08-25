4746343d10d1547cd877a27596f0af39d17fb894
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 11:58:55 +0200

    Add function for solving given problem

--- lib/genetic_algorithm.ex
+++ lib/genetic_algorithm.ex
@@ -5,6 +5,19 @@ defmodule GeneticAlgorithm do
   @mutation_chance 0.1
   @chromosomes_in_gen 500
 
+  def find_solution(fitness_func, min_fitness),
+    do: _find_solution(generate_initial_population(fitness_func), fitness_func, min_fitness, 0)
+
+  def _find_solution(population, fitness_func, min_fitness, counter) do
+    unless can_stop?(population, fitness_func, min_fitness) do
+      counter = counter + 1
+      gen = next_generation(population, fitness_func)
+      _find_solution(gen, fitness_func, min_fitness, counter)
+    else
+      %{generations: counter, most_fitting: select_most_fitting(population, 3)}
+    end
+  end
+
   def generate_initial_population(fitness_func),
     do: populate(@chromosomes_in_gen, @gene_values, 5, fitness_func)
 

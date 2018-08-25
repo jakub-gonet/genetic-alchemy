790e7a29d47d06f63a9315053a3d110a69abed33
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 19:14:34 +0200

    Add Chromosome @moduledoc

--- lib/chromosome.ex
+++ lib/chromosome.ex
@@ -1,3 +1,9 @@
 defmodule Chromosome do
+@moduledoc """
+This struct is representing single Chromosome.
+It contains two keys: `genes` and `fitness`.
+`genes` is used for storing Genes comprising into chromosome data.
+`fitness` stores overall Chromosome fitness.
+"""
   defstruct genes: [], fitness: 0
 end

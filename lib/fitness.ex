8d97e7a05667e54cf0cbc9e7fba8589e6d5ec5d1
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 10:53:14 +0200

    Remove Fitness module

--- lib/fitness.ex
+++
@@ -1,8 +0,0 @@
-defmodule Fitness do
-  @moduledoc """
-  This module is responsible for providing function calculate/1
-  which calculates fittness of given Chromosome
-  """
-
-  def calculate(chromosome = %Chromosome{}), do: 1
-end

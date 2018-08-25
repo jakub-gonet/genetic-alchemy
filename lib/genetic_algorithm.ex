b24c7f06892e0c74d744496750e401c4846e6f02
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 11:57:34 +0200

    Add function for generating population

--- lib/genetic_algorithm.ex
+++ lib/genetic_algorithm.ex
@@ -1,4 +1,12 @@
 defmodule GeneticAlgorithm do
+  import GeneticAlgorithm.Helpers
+
+  @gene_values [0, 1]
+  @chromosomes_in_gen 500
+
+  def generate_initial_population(fitness_func),
+    do: populate(@chromosomes_in_gen, @gene_values, 5, fitness_func)
+
   def can_stop?(population, fitness_func, min_fitness) do
     Enum.at(population, 0).fitness >= min_fitness
   end

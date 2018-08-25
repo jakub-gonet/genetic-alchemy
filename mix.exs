ba97ce32baea4fdbce8e4d79c42a42857bbf70d7
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-25 20:00:25 +0200

    Add ExDoc to generate docs

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Genetic.MixProject do
     [
       {:flow, "~> 0.14"},
       {:excoveralls, "~> 0.9", only: :test},
-      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
